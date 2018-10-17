a909173c1c0f8e33f469a95275bb10a45c8ab7fb
Benoît Guigal <benoit.guigal@gmail.com>
2018-10-17 11:24:51 +0200

    Search results (#15)
    * search results working
    
    * sort search results by descending order of links

--- app/src/components/App.vue
+++ app/src/components/App.vue
@@ -2,7 +2,10 @@
   <div id="app">
     <search></search>
     <!-- <filter-list></filter-list> -->
-    <drawer></drawer>
+    <drawer v-if="showDrawer">
+      <drawer-search-results v-if="showDrawerSearchResults"></drawer-search-results>
+      <drawer-node-info v-else-if="showDrawerNodeInfo"></drawer-node-info>
+    </drawer>
     <graph></graph>
     <zoom-widget v-if="showGraphWidgets"></zoom-widget>
     <the-button-download v-if="showGraphWidgets"></the-button-download>
@@ -14,6 +17,8 @@
 import Search from "./Search.vue";
 import FilterList from "./FilterList.vue";
 import Drawer from "./Drawer.vue";
+import DrawerNodeInfo from "./DrawerNodeInfo";
+import DrawerSearchResults from "./DrawerSearchResults";
 import Graph from "./Graph.vue";
 import ZoomWidget from "./ZoomWidget.vue";
 import TheButtonDownload from "./TheButtonDownload.vue";
@@ -23,13 +28,14 @@ import { mapActions, mapState, mapGetters } from 'vuex';
 
 export default {
   name: "app",
-  components: {Search, FilterList, Drawer, Graph, ZoomWidget, TheButtonDownload, ProgressSpinner },
+  components: { Search, FilterList, Drawer, Graph, ZoomWidget, TheButtonDownload, 
+    ProgressSpinner, DrawerNodeInfo, DrawerSearchResults },
   computed: {
     filter() {
       return this.$store.state.filter;
     },
-    ...mapState(["showModal", "showProgressSpinner"]),
-    ...mapGetters(["showGraphWidgets"])
+    ...mapState(["showModal", "showProgressSpinner", "showDrawerSearchResults"]),
+    ...mapGetters(["showGraphWidgets", "showDrawer", "showDrawerNodeInfo"])
   },
   methods: {
     ...mapActions(['expand'])
--- app/src/components/Search.vue
+++ app/src/components/Search.vue
@@ -1,17 +1,30 @@
 <template>
-  <md-autocomplete id="search" v-model="value" :md-options="people" md-layout="box" @md-changed="search" autofocus @md-selected="handleSelected" v-bind:class="{ 'search-visible': isVisible, 'search-hidden': !isVisible }"> 
-    <label>Rechercher</label>
-    <md-icon id="ic-search">search</md-icon>
-    <template slot="md-autocomplete-item" slot-scope="{ item, term }">
-      {{ item.prenom_nom + " #" + item.entity}}
-    </template>
-  </md-autocomplete>
+  <div id="search-container" v-on:keyup.enter="handleSubmit">
+    <md-autocomplete id="search" 
+      v-model="value" 
+      :md-options="people" 
+      md-layout="box" 
+      @md-changed="handleChange" 
+      autofocus 
+      @md-selected="handleSelected" 
+      v-bind:class="{ 'search-visible': isVisible, 'search-hidden': !isVisible }"> 
+      <label>Rechercher</label>
+      <md-icon id="ic-search">search</md-icon>
+      <template slot="md-autocomplete-item" slot-scope="{ item, term }">
+        {{ item.prenom_nom + " #" + item.entity}}
+      </template>
+    </md-autocomplete>
+    <div id="back-to-results" v-if="showBackToResults" @click="SHOW_DRAWER_SEARCH_RESULTS">
+      <a>Retours aux résultats</a>
+    </div>
+  </div>
 </template>
 
 <script>
 import api from "../api";
-import { mapActions } from 'vuex';
+import { mapActions, mapGetters, mapMutations } from 'vuex';
 import { debounce } from '../utils';
+import { UPDATE_SEARCH_RESULTS, SHOW_DRAWER_SEARCH_RESULTS, HIDE_DRAWER_SEARCH_RESULTS } from '../mutation-types';
 
 const debouncedSearch = debounce(api.search, 800, {leading: true});
 
@@ -24,16 +37,30 @@ export default {
       people: []
     };
   },
+  computed: {
+    ...mapGetters(['showBackToResults'])
+  },
   methods: {
-    search(searchTerm) {
-      const options = { params: { search_term: searchTerm } }
+    handleChange(searchTerm) {
+      if (searchTerm === "") {
+        this.UPDATE_SEARCH_RESULTS([]);
+      }
+      const options = { params: { search_term: searchTerm } };
       this.people = debouncedSearch(options);
     },
     handleSelected(person){
+      this.UPDATE_SEARCH_RESULTS([]);
       this.value = person.prenom_nom;
-      this.expand(person.entity)
+      this.expand(person.entity);
     },
-    ...mapActions(['expand'])
+    handleSubmit(){
+      document.activeElement.blur();
+      if (this.value != "") {
+        this.search(this.value);
+      }
+    },
+    ...mapActions(['expand', 'search']),
+    ...mapMutations([UPDATE_SEARCH_RESULTS, SHOW_DRAWER_SEARCH_RESULTS])
   },
   mounted() {
     const vm = this;
@@ -50,10 +77,31 @@ export default {
 <style lang="scss">
 @import "../scss/settings.scss";
 
-#search {
+#search-container {
   position: absolute;
   left: 16px;
   top: 16px;
+}
+
+#back-to-results {
+  position: relative;
+  top: -24px;
+  z-index: 3;
+  border-left: solid 1px $silver;
+  border-right: solid 1px $silver;
+  border-bottom: solid 1px $silver;
+  padding: 4px;
+  font-size: 12px;
+  color: $peter-river;
+  background-color: white;
+}
+
+#back-to-results:hover {
+  cursor: pointer;
+  text-decoration: underline;
+}
+
+#search {
   width: 350px;
   box-shadow: none !important;
   border: solid 1px $silver;
